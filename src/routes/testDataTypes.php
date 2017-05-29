7388b2a9dbd70caa29956ea595075af9d9867ee1
ustal <quit.ipad2@gmail.com>
2017-05-29 15:51:37 +0300

    attemp list of select and view request from user

--- src/routes/testDataTypes.php
+++ src/routes/testDataTypes.php
@@ -7,6 +7,7 @@ $app->post('/api/MapboxDuration/testDataTypes', function ($request, $response, $
 
     $settings = $this->settings;
     $checkRequest = $this->validation;
+    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($request->getBody());
     $validateRes = $checkRequest->validate($request, ['accessToken']);
     if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
