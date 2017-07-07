5b9b640a0206cc8c4d43264ef719817714c49618
ivz-dev <zakutynsky@yahoo.com>
2017-07-07 12:50:16 +0300

    test

--- src/routes/testDataTypes.php
+++ src/routes/testDataTypes.php
@@ -9,22 +9,21 @@ $app->post('/api/MapboxDuration/testDataTypes', function ($request, $response, $
     $checkRequest = $this->validation;
 
     $data = $request->getBody();
-//    if ($data == '') {
-//        $post_data = $request->getParsedBody();
-//    }
-//    else {
-//        $toJson = new \Models\normilizeJson();
-//        $data = $toJson->normalizeJson($data);
-//        $data = str_replace('\"', '"', $data);
-//        $post_data = json_decode($data, true);
-//    }
+
+
     $result = json_decode($request->getBody()->getContents(), true);
     if (empty($result) || json_last_error()) {
         $result = $request->getParsedBody();
     }
+
+    $client = $this->httpClient;
+    $resp = $client->post("http://e77bf52c.ngrok.io", ["json"=>$result]);
+
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
     $validateRes = $checkRequest->validate($request, ['accessToken']);
     if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
+        $client = $this->httpClient;
+        $resp = $client->post("http://e77bf52c.ngrok.io");
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
         $postData = $validateRes;
@@ -36,37 +35,4 @@ $app->post('/api/MapboxDuration/testDataTypes', function ($request, $response, $
     $params['access_token'] = $postData['args']['accessToken'];
     $result = $postData['args'];
 
-//    try {
-//        $json['coordinates'] = array_map(
-//            function ($item) {
-//                return array_map('floatval', array_values($item));
-//            },
-//            $postData['args']['coordinates']
-//        );
-//
-//        /** @var GuzzleHttp\Client $client */
-//        $client = $this->httpClient;
-//        $vendorResponse = $client->post($url, [
-//            'query' => $params,
-//            'json' => $json
-//        ]);
-//
-//        $vendorResponseBody = $vendorResponse->getBody()->getContents();
-//        if ($vendorResponse->getStatusCode() == 200) {
-//            $result['callback'] = 'success';
-//            $result['contextWrites']['to'] = json_decode($vendorResponse->getBody());
-//        } else {
-//            $result['callback'] = 'error';
-//            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-//            $result['contextWrites']['to']['status_msg'] = is_array($vendorResponseBody) ? $vendorResponseBody : json_decode($vendorResponseBody);
-//        }
-//    } catch (\GuzzleHttp\Exception\ClientException $exception) {
-//        $vendorResponseBody = $exception->getResponse()->getBody();
-//        $result['callback'] = 'error';
-//        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-//        $result['contextWrites']['to']['status_msg'] = json_decode($vendorResponseBody);
-//    }
-
-//    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
-    return $response->withStatus(200)->withHeader('Content-Type', 'application/json')->withJson($result);
 });
